bfde0125c7e9f5315c54cb9d13141a897bb49cfd
DrLivisey <mahinov.egor@yandex.ru>
2023-11-22 13:57:10 +0300

    test3

--- build/CMakeFiles/CMSIS_LD_L433C8.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/CMSIS_LD_L433C8"
-  "L433C8.ld"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CMSIS_LD_L433C8.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/aqbm1000ll_always_display_size.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for aqbm1000ll_always_display_size.
